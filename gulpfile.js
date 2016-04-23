b4382c12767bdce9749dc590de7133a37180d262
Developoz <trung_vo_6@hotmail.com>
2016-04-23 11:10:35 +0200

    added gulp task in

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var jade        = require('gulp-jade');
 
 // Static Server + watching scss/html and js files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass']['templates'], function() {
 
     browserSync.init({
         server: "./app"
     });
-
+    gulp.watch("app/*.jade", ['templates']);
     gulp.watch("app/sass/*.scss", ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
     gulp.watch("app/js/**/*.js", browserSync.reload); 
@@ -21,5 +22,19 @@ gulp.task('sass', function() {
         .pipe(gulp.dest("./app/css"))
         .pipe(browserSync.stream());
 });
+
+// Compile jade into html
+
+gulp.task('templates', function(){
+    var YOUR_LOCALS = {};
+
+    gulp.src('./app/*.jade')
+    .pipe(jade({
+        locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('./app/'))
+    .pipe(browserSync.stream());
+});
+
 
 gulp.task('default', ['serve']);
